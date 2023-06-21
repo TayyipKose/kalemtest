4ee088b3080bee822e4958e6f277331e654f85b2
Tayyip Köse <tayyipkose037@gmail.com>
2023-06-21 17:02:33 +0300

    commit-39.3

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Cart } from '../module/cart-model/cartmodel';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class LoginService {
   private apiUrl = 'http://localhost:9000/api/login';
   isLoggedIn: boolean = localStorage.getItem('isLoggedIn') === 'true';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private route: Router) { }
 
   login(username: string, password: string): Observable<any> {
     this.isLoggedIn = true;
@@ -21,6 +22,7 @@ export class LoginService {
   logout() {
     this.isLoggedIn = false;
     localStorage.setItem('isLoggedIn', 'false');
+    this.route.navigateByUrl('/');
   }
 
   isAuthenticated(): boolean {
